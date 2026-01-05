72445cc2d7a4570e92b6bc0a5728b26c46548ed7
manishakr7209-ui <manishakr7209@gmail.com>
2026-01-05 22:30:28 +0530

    backend

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import flask, jsonify
+from data import country_data
+app = flask(__name__)
+@app.route("/")
+def home():
+    return "World Dashboard Backend Running"
+
+@app.route("/country/<name>")
+def get_country(name):
+    name = name.lower()
+    if name in country_data:
+        return jsonify(country_data[name])
+    else:
+        return jsonify({"error": "Country not found"}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,16 @@
+country_data={
+    "india":{
+        "population":"1.4 Billion",
+        "birth_rate":"17 per 1000",
+        "death_rate":"7 per 1000",
+        "life_expectancy":"69 years",
+        "description": "India is the most populous country with rich cultural diversity."
+    },
+    "usa": {
+        "population": "331 Million",
+        "birth_rate": "12 per 1000",
+        "death_rate": "8 per 1000",
+        "life_expectancy": "78 years",
+        "description": "USA has a developed healthcare and economic system."
+    }
+}
